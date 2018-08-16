3cddd982062ad5427aa4f5bb13258eb594fd8667
Matthew Krasnick <matthew.krasnick@gmail.com>
2018-08-16 14:39:17 -0400

    Fix to remove password_digest field

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_05_134354) do
+ActiveRecord::Schema.define(version: 2018_08_16_171406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "email"
-    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "encrypted_password", default: "", null: false
